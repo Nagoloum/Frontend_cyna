93320a0719e99a53e36a19a09b9af9a68ce02a38
Nagoloum <landhack049@gmail.com>
2026-03-06 15:38:11 +0100

    feat: remove authentication requirement from HomePage route

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
         <Route
           path="/home"
           element={
-            <RouteLayout requireAuth={true}>
+            <RouteLayout>
               <Layout>
                 <HomePage />
               </Layout>
